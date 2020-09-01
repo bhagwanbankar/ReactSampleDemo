31d30f2e7d6440cf9d50415990ed610d335a0c12
bhagwanbankar <bhagwan.bankar312@gmail.com>
2020-09-01 23:26:16 +0530

    order summary added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -68,6 +68,11 @@ class BurgerBuilder extends Component{
     purchaseCancelHandler = () => {
         this.setState({purchasing:false});
     }
+    purchaseContinueHandler = () => {
+        //this.setState({purchasing:false});
+        alert("continue clicked");
+    }
+
     render(){
         const disabledInfo = {
             ...this.state.ingredients
@@ -78,7 +83,11 @@ class BurgerBuilder extends Component{
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
-                    <OrderSummary ingredients = {this.state.ingredients}></OrderSummary>
+                    <OrderSummary ingredients = {this.state.ingredients}
+                    purchaseCanceled = {this.purchaseCancelHandler}
+                    purchaseContinued = {this.purchaseContinueHandler}
+                    price = {this.state.totalPrice}
+                    />
                 </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls
